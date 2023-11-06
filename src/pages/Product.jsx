5dbf5a8c7e7374c44ffb61ce9ef574fd9dc247e6
minhtai <dangminhtai15052002@gmail.com>
2023-11-06 14:27:49 +0700

    update file 6/11

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
 import { useDocsQuery } from '@/hooks/useFirestore';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { auth, firestore } from '@/utils/firebase.config';
-import ProductLayout from '@/layout/Product'
+import ProductLayout from '@/layout/Product';
 
 const Product = () => {
-    const {data: luuTru} = useDocsQuery('luuTru');
-    const renderStars = (soSao) => {
+	const { data: luuTru } = useDocsQuery('luuTru');
+	const renderStars = (soSao) => {
 		let stars = [];
 		for (let i = 0; i < soSao; i++) {
 			stars.push(
@@ -18,14 +15,14 @@ const Product = () => {
 		}
 		return stars;
 	};
-    return (
-        <>
-        <ProductLayout 
-            luuTru = {luuTru}
-            renderStars = {renderStars}
-        />
-        </>
-    );
+	return (
+		<>
+			<ProductLayout
+				luuTru={luuTru}
+				renderStars={renderStars}
+			/>
+		</>
+	);
 };
 
 export default Product;
