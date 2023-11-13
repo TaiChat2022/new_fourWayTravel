47411f54d7f71840fa004c6e7154a52abafa1d30
minhtai <dangminhtai15052002@gmail.com>
2023-11-13 13:46:26 +0700

    update 13/11

--- src/pages/chiTiet.jsx
+++ src/pages/chiTiet.jsx
@@ -30,15 +30,66 @@ const chiTiet = () => {
 		return stars;
 	};
 
+	//tiện ích
+	// const tienIchIcons = {
+	// 	DieuHoa: 'fa-snowflake',
+	// 	pool: 'fa-swimming-pool',
+	// 	wifi: 'fa-wifi',
+	// };
 	const checkIcon = (tienIch) => {
-		if (tienIch === "Điều hòa ") {
+		if (tienIch === 'Điều hòa') {
 			return 'fa-snowflake';
 		}
-		if (tienIch === "WiFi") {
+		if (tienIch === 'Với đầy đủ bữa ăn') {
+			return 'fa-utensils';
+		}
+		if (tienIch === 'Đường cho xe lăn') {
+			return 'fa-wheelchair';
+		}
+		if (tienIch === 'Thanh toán tại nơi lưu trú') {
+			return 'fa-money-bill';
+		}
+		if (tienIch === 'Với 1 bữa ăn') {
+			return 'fa-bell-concierge';
+		}
+		if (tienIch === 'Đầy đủ dịch vụ') {
 			return 'fa-wifi';
 		}
+		if (tienIch === 'Gia đình') {
+			return 'fa-people-roof';
+		}
+		if (tienIch === 'Biển') {
+			return 'fa-water';
+		}
+		if (tienIch === 'Bể bơi') {
+			return 'fa-water-ladder';
+		}
+		if (tienIch === 'Gồm bữa sáng') {
+			return 'fa-utensils';
+		}
+		if (tienIch === 'Đậu xe') {
+			return 'fa-square-parking';
+		}
+		if (tienIch === 'Bồn tắm nước nóng') {
+			return 'fa-bath';
+		}
+		if (tienIch === 'Spa') {
+			return 'fa-spa';
+		}
+		if (tienIch === 'Miễn phí Đổi/Hủy') {
+			return 'fa-road-circle-check';
+		}
+		if (tienIch === 'Chấp nhận vật nuôi') {
+			return 'fa-paw';
+		}
+		if (tienIch === 'Nhà hàng') {
+			return 'fa-utensils';
+		}
+		if (tienIch === 'Phòng Gym') {
+			return 'fa-dumbbell';
+		}
 		return null;
-	}
+	};
 
 	return (
 		<>
